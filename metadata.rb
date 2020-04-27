baddd09ec8a9ebccd301d4fe2f20a7a0e68abcc0
ronigarcia <sgarcia.roni@gmail.com>
2020-04-27 20:11:24 -0300

    teste travis

--- metadata.rb
+++ metadata.rb
@@ -1,19 +1,21 @@
 name 'install-apache'
 maintainer 'Roni Garcia'
 maintainer_email 'sgarcia.roni@gmail.com'
-license 'All Rights Reserved'
+license 'Apache-2.0'
 description 'Installs/Configures install-apache'
-version '0.1.0'
+version '0.1.1'
 chef_version '>= 14.0'
+supports 'ubuntu', '>=14.04'
+license 'Apache-2.0'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
-# issues_url 'https://github.com/<insert_org_here>/install-nginx/issues'
+issues_url 'https://github.com/https://github.com/ronigarcia/chef-kitchen-circleci/issues'
 
 # The `source_url` points to the development repository for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-# source_url 'https://github.com/<insert_org_here>/install-nginx'
+source_url 'https://github.com/<insert_org_here>/install-nginx'
